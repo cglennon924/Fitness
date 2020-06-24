cc834f4f9fcf64dc070f8fb7274a3092be844f9b
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-06-24 17:21:32 -0400

    Created app.use functions

--- server.js
+++ server.js
@@ -6,3 +6,9 @@ const path = require("path");
 // Creating a PORT and requiring models to sync
 const PORT = process.env.PORT || 3000;
 const db = require("./models");
+
+const app = express();
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
