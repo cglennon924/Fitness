bae87ef9f9761c98d9d939e6acec8d9b498c642a
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-06-24 21:46:17 -0400

    created

--- models/workout.js
+++ models/workout.js
@@ -1,2 +1,43 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    type: {
+        type: String,
+        trim: true
+    },
+    name: {
+        type: String,
+        trim: true
+    },
+    weight: {
+        type: Number,
+        trim: true
+    },
+    sets: {
+        type: Number,
+        trim: true
+
+    },
+    reps: {
+        type: Number,
+        trim: true
+    },
+    distance: {
+        type: Number,
+        trim: true
+    },
+    duration: {
+        type: Number,
+        trim: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: []
+}, {
+    toJSON: {
+        virtuals: true
+    }
+});
